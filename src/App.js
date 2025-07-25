8d596bbc1f39c0862bc441a94ee599aac5079cfd
dolly09082 <dp767893@gmail.com>
2025-07-25 15:14:52 +0530

    New Changes

---
+++ src/App.js
@@ -0,0 +1,19 @@
+//import logo from './logo.svg';
+import './App.css';
+import About from "./screens/About"
+
+function App() {
+
+  return <About/>;
+
+    
+}
+
+export default App;
+//Basic App Create - Done
+//Screens and Components -Organize
+
+//App.js -> src/
+//screems/About.js
+// ./ -Current Directory
+// ../ - to move to a directory above
